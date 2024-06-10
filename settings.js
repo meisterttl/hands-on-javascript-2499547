085dff4b7505d624cc059e44d8b59561cf1ed438
Taylor <meisterttl@gmail.com>
2024-06-10 22:24:51 +0000

    Fixing

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: "22466d25b513efb5699b196397a768ab",
+  appid: "",
   units: "metric",
   location: "vancouver,ca",
 };
